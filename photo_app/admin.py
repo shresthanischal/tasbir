43903ad7bc94a50e87599f2842a97c7d860eb469
shresthanischal <shresthanischal98@gmail.com>
2019-12-20 10:39:14 +0545

    V2

--- photo_app/admin.py
+++ photo_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import PostModel, CommentModel
+
+admin.site.register(PostModel)
+admin.site.register(CommentModel)
+
--- user_app/urls.py
+++ user_app/urls.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path(),
+
 ]
